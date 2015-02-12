7f3b12745a2e59e4e4e9c720b619669e459e403a
Thomas Bauers <tmoss343@gmail.com>
2015-02-11 18:19:07 -0600

    Getting the bare bones of movement then making a test player controller for basic movement.

---
+++ SoteriaGame/Assets/Movement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement {
+
+    public void Move(float inputX, float inputY, float inputZ, float speed, Transform whatToTransform)
+    {
+        whatToTransform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed));
+    }
+}
---
+++ SoteriaGame/Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    Movement myMovementComponents;
+	// Use this for initialization
+	void Start () {
+        myMovementComponents = new Movement();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myMovementComponents.Move(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"), moveSpeed, this.transform);
+	}
+}
