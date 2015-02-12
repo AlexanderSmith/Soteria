ac65ef04337277a4c27d2f661cc7a85763a719fd
Thomas Bauers <tmoss343@gmail.com>
2015-02-11 23:02:45 -0600

    Adding a very simple range based AI that will notice at a radial range then attack at a radial range. Right now changes color and updates the object within the check. This should change and it should return which state the AI should enter into. For now in the basic demo this should be okay.

---
+++ SoteriaGame/Assets/BasicAggro.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAggroSystem {
+
+     public void AggroCheckAndBasicMove (float lookAtDistance, float attackRange, Transform target, Transform seeker) 
+     {
+         Debug.Log("TARGET"+target.position);
+         Debug.Log("SEEKER"+seeker.position);
+         float distance = Vector3.Distance(target.position, seeker.position);
+ 
+         if(distance < lookAtDistance)
+         {
+             seeker.renderer.material.color = Color.yellow;
+             seeker.LookAt(target);
+         }   
+         if(distance > lookAtDistance)
+         {
+            seeker.renderer.material.color = Color.green; 
+         }
+         if(distance < attackRange)
+         {
+             target.position += target.forward * Time.deltaTime * 0.3f;//arb speed
+             seeker.renderer.material.color = Color.red;
+         }
+     }
+}
