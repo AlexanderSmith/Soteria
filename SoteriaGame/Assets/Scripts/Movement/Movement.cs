5aa3534b9fdf6211cc7f5c2607b4b23db1847e87
Thomas Bauers <tmoss343@gmail.com>
2015-03-02 18:47:22 -0600

    Adding some stuff so I can work out my master error with Wesley.

--- SoteriaGame/Assets/Scripts/Movement/Movement.cs
+++ SoteriaGame/Assets/Scripts/Movement/Movement.cs
@@ -5,16 +5,22 @@ public class Movement {
 
     public void Move(Vector3 input, float speed, Transform whatToTransform)
     {
-        whatToTransform.Translate(input * speed);
-        MakeTransformLookAtMoveDirection(whatToTransform, input);
+        Vector3 relativeInput = input;//Camera.main.transform.TransformDirection(input);
+        Vector3 prevPos = whatToTransform.position;
+        Debug.Log(relativeInput);
+        
+        whatToTransform.rigidbody.MovePosition((relativeInput * speed) + whatToTransform.position);//.rigidbody.MovePosition(relativeInput * speed);
+        
+        whatToTransform.Rotate(whatToTransform.up, Vector3.Angle(prevPos, whatToTransform.position));
+        //MakeTransformLookAtMoveDirection(whatToTransform, input);
     }
 
     private void MakeTransformLookAtMoveDirection(Transform whatToTransform, Vector3 inputVec)
     {
         // Rotation
-        /*if (inputVec != Vector3.zero)
+        if (inputVec != Vector3.zero)
             whatToTransform.GetChild(0).transform.rotation = Quaternion.Slerp(whatToTransform.GetChild(0).transform.rotation,
                 Quaternion.LookRotation(inputVec),
-                Time.deltaTime * 20.0f);*/
+                Time.deltaTime * 20.0f);//*/
     }
 }
