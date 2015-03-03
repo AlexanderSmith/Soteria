9a147645d739b30a5e193fca65545fc85a5fda4a
Thomas Bauers <tmoss343@gmail.com>
2015-03-02 19:03:21 -0600

    Made it so the character moves only one direction when you press W no matter the rotation. Also made the character rotate the direction of the keys you are pressing.

--- SoteriaGame/Assets/Scripts/Movement/Movement.cs
+++ SoteriaGame/Assets/Scripts/Movement/Movement.cs
@@ -4,22 +4,17 @@ using System.Collections;
 public class Movement {
 
     public void Move(Vector3 input, float speed, Transform whatToTransform)
-    {
-        Vector3 relativeInput = input;//Camera.main.transform.TransformDirection(input);
-        Vector3 prevPos = whatToTransform.position;
-        Debug.Log(relativeInput);
-        
-        whatToTransform.rigidbody.MovePosition((relativeInput * speed) + whatToTransform.position);//.rigidbody.MovePosition(relativeInput * speed);
-        
-        whatToTransform.Rotate(whatToTransform.up, Vector3.Angle(prevPos, whatToTransform.position));
-        //MakeTransformLookAtMoveDirection(whatToTransform, input);
+    {        
+        whatToTransform.rigidbody.MovePosition((input * speed) + whatToTransform.position);
+
+        MakeTransformLookAtMoveDirection(whatToTransform, input);
     }
 
     private void MakeTransformLookAtMoveDirection(Transform whatToTransform, Vector3 inputVec)
     {
         // Rotation
         if (inputVec != Vector3.zero)
-            whatToTransform.GetChild(0).transform.rotation = Quaternion.Slerp(whatToTransform.GetChild(0).transform.rotation,
+            whatToTransform.rotation = Quaternion.Slerp(whatToTransform.rotation,
                 Quaternion.LookRotation(inputVec),
                 Time.deltaTime * 20.0f);//*/
     }
