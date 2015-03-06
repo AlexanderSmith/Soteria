33e038d14fce29cf15f31f02df3c76aeaea86910
Thomas Bauers <tmoss343@gmail.com>
2015-03-05 19:48:10 -0600

    Adding controllers for the statue and fear residue so they will kill themselves when you collide with it. Also soteria statue controller reloads the scene for now.

---
+++ SoteriaGame/Assets/Scripts/Controllers/SoteriaStatueController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoteriaStatueController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Destroy(this);
+        Application.LoadLevel("Basic");
+    }
+}
