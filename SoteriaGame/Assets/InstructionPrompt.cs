42b10e81d9f9f7735040631b84741adc086426ca
FierceLlama <sosaca54@yahoo.com>
2015-03-27 18:16:31 -0500

    Fixing errors in previous commit

--- SoteriaGame/Assets/InstructionPrompt.cs
+++ SoteriaGame/Assets/InstructionPrompt.cs
@@ -1,35 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class InstructionPrompt : MonoBehaviour {
 	
-	public float timeDelay = 5;
+	public float timeDelay = 10;
 	
 	// Use this for initialization
 	void Start () 
 	{
+		float timeLeft = 30.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (GameObject.Find("Player").transform.position.x > 93.0f)
-		{
-		//	Debug.Log ("OH yEAH");
-			
-			this.GetComponent<Text>().text = "Hide Behind the Wall to Avoid the Shadow Creatures!";
-			this.gameObject.SetActive(true);
-			if (timeDelay == 0) timeDelay = 5;	
-		}
-
 		timeDelay -= Time.deltaTime;
 		if ( timeDelay < 0 )
 		{
-			this.gameObject.SetActive(false);
-			//GameObject X =  GameObject.FindGameObjectWithTag("InstructionPrompt");
-			//X.SetActive(false);
+			GameObject X =  GameObject.FindGameObjectWithTag("InstructionPrompt");
+			X.SetActive(false);
 		}
-
 	}
 }
--- SoteriaGame/Assets/Scripts/Movement/Movement.cs
+++ SoteriaGame/Assets/Scripts/Movement/Movement.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class Movement {
 
     public void Move(Vector3 input, float speed, Transform whatToTransform)
-    {		
+    {
+        whatToTransform.rigidbody.velocity = new Vector3(0, 0, 0);
         whatToTransform.rigidbody.MovePosition((input * speed) + whatToTransform.position);
-
+        
         MakeTransformLookAtMoveDirection(whatToTransform, input);
     }
 
