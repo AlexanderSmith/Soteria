bd87d763962be45f6061eb935f775249ccdfb984
FierceLlama <sosaca54@yahoo.com>
2015-03-28 06:55:55 -0500

    Safety light added an implemented.  Player still requires a change of movement from encounter to standard after statue token GUI used.

--- SoteriaGame/Assets/Scripts/Controllers/SafetyLightController.cs
+++ SoteriaGame/Assets/Scripts/Controllers/SafetyLightController.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class SafetyLightController : MonoBehaviour {
 
+	GameObject safeArea;
+
+	public enum State
+	{
+		Finding,
+		Moving
+	};
+	State currentState;
+
+	float lightSpeed = 1f;
+
 	// Use this for initialization
 	void Start () {
-	
+		currentState = State.Finding;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (currentState == State.Finding) 
+		{
+			FindNextClosest ();	
+		}
+
+		if (currentState == State.Moving) 
+		{
+			this.transform.position = Vector3.MoveTowards(this.transform.position, safeArea.transform.position, Time.deltaTime * lightSpeed);
+			if (Vector3.Distance(this.transform.position, safeArea.transform.position) <= 2f)
+			{
+				currentState = State.Finding;
+			}
+		}
+	}
+
+	void FindNextClosest()
+	{
+		safeArea = GameObject.FindWithTag ("SafeArea");
+		currentState = State.Moving;
 	}
 }
