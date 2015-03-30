09905d741318c782349a6808d0f5e5dbffbbd778
Chris Sosa <sosaca54@yahoo.com>
2015-03-30 02:57:02 -0500

    Idle cycle added.  Transition between idle and run not working correctly.  Pushing for code to be seen by Omar.

--- SoteriaGame/Assets/Scripts/Controllers/StandardMovementController.cs
+++ SoteriaGame/Assets/Scripts/Controllers/StandardMovementController.cs
@@ -8,20 +8,23 @@ public class StandardMovementController : MonoBehaviour {
 
     Quaternion overwhelmedRotation;
     Movement myMovementComponents;
+	Animator animator;
 
     // Use this for initialization
     void Start()
     {
         myMovementComponents = new Movement();
+		animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         myMovementComponents.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), moveSpeed, this.transform);
+		animator.SetFloat("Speed", Input.GetAxis("Horizontal"));
     }
 
-    void OnMouseDown()
-    {
-    }
+//    void OnMouseDown()
+//    {
+//    }
 }
