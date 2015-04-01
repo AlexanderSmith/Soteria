149ab47c3bd3607217acda1908edf9386e6239cb
Chris Sosa <sosaca54@yahoo.com>
2015-03-31 21:29:38 -0500

    Pulse added to Soteria token during encounter.  Implemented by Chris Alexander...I'm just pushing.

--- SoteriaGame/Assets/Scripts/Controllers/PCController.cs
+++ SoteriaGame/Assets/Scripts/Controllers/PCController.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class PCController : MonoBehaviour {
 
-    public GameObject button;
+    private ButtonController button;
     StandardMovementController stdMovement;
     EncounterMovementController encMovement;
+
+	//removing public GameObject reference will break rest of script
+	ButtonController buttonController;
 
 	// Use this for initialization
 	void Start () {
         stdMovement = gameObject.GetComponent<StandardMovementController>();
         encMovement = gameObject.GetComponent<EncounterMovementController>();
-        EnableStandardMovement();
+        
+		button = GameObject.Find ("Button").GetComponent<ButtonController> ();
+
+		EnableStandardMovement();
 	}
 	
 	// Update is called once per frame
@@ -23,12 +29,15 @@ public class PCController : MonoBehaviour {
         stdMovement.enabled = false;
         encMovement.enabled = true;
 
+		button.Pulsing = true;
+
     }
 
     public void EnableStandardMovement()
     {
         stdMovement.enabled = true;
         encMovement.enabled = false;
-    }
 
+		button.Pulsing = false;
+    }
 }
