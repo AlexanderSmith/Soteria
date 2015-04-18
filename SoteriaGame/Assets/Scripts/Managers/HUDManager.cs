fc4c8344cbd987576fdd380dbe2501986bd116d9
Thomas Bauers <tmoss343@gmail.com>
2015-04-18 11:48:25 -0500

    wide first pass on the systems to enable an encounter working on getting the HUD to set up for normal play right now unity is fighting me on runtime object instantiation.

--- SoteriaGame/Assets/Scripts/Managers/HUDManager.cs
+++ SoteriaGame/Assets/Scripts/Managers/HUDManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class HUDManager : MonoBehaviour {
 
+    private GUIText instructionText;
+    private GUITexture coin;
+    private GUITexture fadeTexture;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,11 +13,34 @@ public class HUDManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    //IF STATEMANAGER IS IN ENCOUNTER MODE
+        //PULSE COIN AND FADE
 	}
 
 	public void Initialize()
 	{
-		
+        instructionText = Instantiate(Resources.Load("GUIText.prefab")) as GUIText;
+        coin = Instantiate(Resources.Load("GUITexture.prefab")) as GUITexture;
+
+        instructionText.transform.position = new Vector3(0.1f, 0.1f, 0);
+        instructionText.text = "Test";
+        
+        coin.transform.position = new Vector3(0.1f, 0.9f, 0);
+        coin.texture = (Texture)Resources.Load("GUI/Soteria_coin.png");
+        
 	}
+
+    public void EnableEncounterView()
+    {
+        fadeTexture.enabled = true;
+        coin.enabled = true;
+        instructionText.enabled = false;
+    }
+
+    public void EnableNormalView()
+    {
+        instructionText.enabled = true;
+        coin.enabled = true;
+        fadeTexture.enabled = false;
+    }
 }
