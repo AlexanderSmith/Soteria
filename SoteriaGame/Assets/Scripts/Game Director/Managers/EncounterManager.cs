3e9b5731aa7347bddf92bfb3ff9da9a8e15e4117
Omar Zohdi <omarzohdi@gmail.com>
2015-05-07 23:36:41 -0500

    Latests Code and systems build
    Input manager and Hud Manager integrated! The Hud Manager was updated to
    use a canvas prefab. Also created a basic Encounter Manager and a Basic
    State Manager.

---
+++ SoteriaGame/Assets/Scripts/Game Director/Managers/EncounterManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Encounter manager.
+/// 
+/// Possible Additions: 
+/// Set the Encounter States -> what's the state of the player: winning, losing, escaping ecc.
+/// 
+/// </summary>
+
+
+public class EncounterManager : MonoBehaviour {
+
+	private bool _startEnc;
+	private bool _encActive;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		this.enabled = false;
+
+		this._encActive = false;
+		this._startEnc = false;
+	}
+
+	// Update is called once per frame
+	public void Update () 
+	{
+		if (this._startEnc && !this._encActive)
+			this.UpdateEncounter(true);
+		else if (this._encActive && !this._startEnc)
+			this.UpdateEncounter(false);
+	}
+
+	public void Initialize()
+	{
+		
+	}
+
+    private void UpdateEncounter(bool inStatus)
+	{
+		this._encActive = inStatus;
+		GameDirector.instance.UpdateEncounterState(this._encActive);
+    }
+
+	public void ActivateEncounter()
+	{
+		this._startEnc = true;
+	}
+
+	public void DeActivateEncounter()
+	{
+		this._startEnc = false;
+	}
+}
