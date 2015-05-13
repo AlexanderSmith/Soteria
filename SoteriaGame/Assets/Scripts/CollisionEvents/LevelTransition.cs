d853a0fd98f7fe13135dbccfad29d4447595ba42
tmoss343 <tmoss343@gmail.com>
2015-05-13 16:29:27 -0500

    Working level manager with transitions between levels triggered by a collider event.

---
+++ SoteriaGame/Assets/Scripts/CollisionEvents/LevelTransition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTransition : MonoBehaviour {
+
+	private LevelManager lvlManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake(){
+		lvlManager = GameObject.Find ("MCP").GetComponent<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(){
+		lvlManager.SetActiveLevel ("SafetyLight");
+	}
+}
