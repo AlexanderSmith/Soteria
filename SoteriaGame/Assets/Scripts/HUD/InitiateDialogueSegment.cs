369e409f528cf7c9a6ab62fe765198451c5776b5
Thomas Bauers <tmoss343@gmail.com>
2015-06-07 14:42:02 -0500

    Burp, forgot some stuff.

--- SoteriaGame/Assets/Scripts/HUD/InitiateDialogueSegment.cs
+++ SoteriaGame/Assets/Scripts/HUD/InitiateDialogueSegment.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class InitiateDialogueSegment : MonoBehaviour {
 
+    public GameObject SpeakerText;
+    public GameObject ResponseOneText;
+    public GameObject ResponseTwoText;
+    public GameObject ResponseThreeText;
+
+    public HUDManager manager;
+
 	// Use this for initialization
 	void Start () {
-	
+        manager = GameObject.Find("HUD").GetComponent<HUDManager>();
+        CycleDiaglogueSegment(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnCollisionStay()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            BeginTextInteraction();
+        }
+    }
+
+    private void BeginTextInteraction()
+    {
+        manager.EnableDialogue();
+        CycleDiaglogueSegment(true);
+    }
+
+    private void CycleDiaglogueSegment(bool value)
+    {
+        SpeakerText.SetActive(value);
+        ResponseOneText.SetActive(value);
+        ResponseTwoText.SetActive(value);
+        ResponseThreeText.SetActive(value);
+    }
 }
