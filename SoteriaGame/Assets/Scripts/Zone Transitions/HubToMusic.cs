4a17c3b21caef83f7a147c3f9d91d225ac6d86c6
FierceLlama <sosaca54@yahoo.com>
2015-08-11 12:14:05 -0500

    Level design walk through build created.

---
+++ SoteriaGame/Assets/Scripts/Zone Transitions/HubToMusic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HubToMusic : MonoBehaviour
+{
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			Application.LoadLevel("WalkThroughMusic");
+		}
+	}
+}
