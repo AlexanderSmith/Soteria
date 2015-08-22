e30e63b26157882095ded961b6cbe385b22acb5d
Tpog <qwek12@hotmail.com>
2015-08-22 13:29:53 -0500

    Import ObsTile - Yuan

---
+++ SoteriaGame/Assets/Scripts/Events&Triggers/ObservatoryTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObservatoryTile : MonoBehaviour
+{
+	void Awake()
+	{
+		this.GetComponentInChildren<Light>().color = Color.cyan;
+	}
+
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			GameDirector.instance.ChangeGameState(GameStates.Hidden);
+			this.GetComponentInChildren<Light>().color = Color.white;
+		}
+	}
+
+	void OnTriggerExit(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			GameDirector.instance.ChangeGameState(GameStates.Normal);
+			this.GetComponentInChildren<Light>().color = Color.cyan;
+		}
+	}
+}
