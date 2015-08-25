f554dd41d8e37563147389feda2e74c0812c1403
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-08-25 16:09:44 -0500

    Fixed lantern for single use and requiring recharge

--- SoteriaGame/Assets/Scripts/Events&Triggers/LanternController.cs
+++ SoteriaGame/Assets/Scripts/Events&Triggers/LanternController.cs
@@ -4,26 +4,45 @@ using System.Collections;
 
 public class LanternController : MonoBehaviour
 {
-	private bool _charged;
+	private bool _charged = false;
+	public int _uses = 1;
 
 	void Start()
 	{
+		Debug.Log (this._charged);
 		this._charged = true;
+		Debug.Log (this._charged);
+	}
+
+	void Update()
+	{
+//		if (!_charged)
+//			Debug.Log ("false");
 	}
 
 	public void LanternClicked()
 	{
+		//Debug.Log (this._charged);
 		// _charged bool not working as intended (semi-works while debugging, but not normally)
 		if (GameDirector.instance.GetGameState() == GameStates.Normal)// && this._charged)
 		{
-			Debug.Log ("Lantern Used");
+			this.UseLantern();
+		}
+	}
+
+	private void UseLantern()
+	{
+		if (this._charged)
+		{
 			GameDirector.instance.LanternUsed();
-			//this._charged = false;
+			this._charged = false;
+			//this._uses--;
 		}
 	}
 
 	public void RechargeLantern()
 	{
 		this._charged = true;
+		this._uses = 1;
 	}
 }
