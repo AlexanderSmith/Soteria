2a69a1abd7856529f60989ff81aa0d4bfb73a98e
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-08-25 15:19:48 -0500

    Eyeball Light mechanic and all associated components added to design branch

---
+++ SoteriaGame/Assets/Scripts/Enemy/EyeballLightController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeballLightController : MonoBehaviour
+{
+	public NavMeshAgent _agent;
+
+//	void Start()
+//	{
+//		_agent = GetComponentInParent<NavMeshAgent>();
+//	}
+
+	void Update()
+	{
+		transform.LookAt (_agent.transform.position);
+	}
+}
---
+++ SoteriaGame/Assets/Scripts/Enemy/EyeballShadowCreatureSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeballShadowCreatureSpawner : MonoBehaviour
+{
+	public float spawnTime = 5.0f;
+	public Transform[] spawnLocations;
+	private int _spawnerIndex;
+	public GameObject enemyPrefab;
+
+	void Start()
+	{
+		InvokeRepeating("ShadowCreatureSpawner", 0.0f, spawnTime);
+		_spawnerIndex = 0;
+	}
+
+	private void ShadowCreatureSpawner()
+	{
+
+		if (_spawnerIndex == spawnLocations.Length - 1)
+		{
+			_spawnerIndex = 0;
+		}
+		else
+		{
+			_spawnerIndex++;
+		}
+		GameObject enemy = Instantiate (enemyPrefab, spawnLocations [_spawnerIndex].position, spawnLocations [_spawnerIndex].rotation) as GameObject;
+//		enemy.GetComponent<BasicEnemyController>().Initialize(GameDirector.instance.GetEncounterManager());
+//		enemy.GetComponent<BasicEnemyController>().GetAgent().SetDestination(GameDirector.instance.GetPlayer().transform.position);
+	}
+}
