bde618af5f1e922c910ef7858a323436139aa433
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-10-10 12:17:57 -0500

    Objectives added up to phase 2.  weird bug with hide and soteria beam near shadow creatures.

---
+++ SoteriaGame/Assets/Scripts/Events&Triggers/ChangeObjNoCompass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeObjNoCompass : MonoBehaviour
+{
+	GameObject lanternObjective;
+	GameObject portToMusic;
+
+	void Start()
+	{
+		lanternObjective = GameObject.Find ("ReceiveLantern");
+		portToMusic = GameObject.Find("HubToMusic");
+	}
+	
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.gameObject.tag == "Player" && GameDirector.instance.GetCompass())
+		{
+			if (!GameDirector.instance.GetLantern())
+			{
+				GameDirector.instance.ChangeObjective(lanternObjective);
+			}
+			else
+			{
+				GameDirector.instance.ChangeObjective(portToMusic);
+			}
+		}
+	}
+}
