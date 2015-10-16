56a097912e258de7b52099260556deccc9cf0a67
FierceLlama <sosaca54@yahoo.com>
2015-10-16 14:13:13 -0500

    Fixed fly through camera to be quicker and more accurate to Doris' idea.

--- SoteriaGame/Assets/Scripts/Entities/Player/FlyThroughSoteriaStatue.cs
+++ SoteriaGame/Assets/Scripts/Entities/Player/FlyThroughSoteriaStatue.cs
@@ -4,23 +4,30 @@ using System.Collections;
 public class FlyThroughSoteriaStatue : MonoBehaviour
 {
 	private GameObject _soteriaStatue;
+	private GameObject _lookLoc;
 	public float distance = 15.0f;
 	private HarborCameraSwitch _cameraSwitch;
+	public float time = 5f;
 
 	public void Initialize(HarborCameraSwitch camSwitch)
 	{
 		_soteriaStatue = GameObject.Find ("SotStat");
+		_lookLoc = GameObject.Find ("CameraShift");
 		_cameraSwitch = camSwitch;
+		this.transform.position = GameDirector.instance.GetPlayer ().transform.position + new Vector3 (0, 15, -15);
+		this.transform.LookAt(GameDirector.instance.GetPlayer().transform);
 	}
 
 	void Update()
 	{
 		if (this.gameObject.GetComponent<FlyThroughSoteriaStatue>().enabled == true)
 		{
-			transform.LookAt (_soteriaStatue.transform);
-			if (Vector3.Distance(this.transform.position, _soteriaStatue.transform.position) > distance)
+			if (/*Vector3.Distance(this.transform.position, _soteriaStatue.transform.position) > distance*/time > 0)
 			{
-				this.transform.position = Vector3.Lerp (this.transform.position, _soteriaStatue.transform.position, .1f * Time.deltaTime);
+				time -= Time.deltaTime;
+				//this.transform.position = Vector3.Lerp (this.transform.position, _soteriaStatue.transform.position, .5f * Time.deltaTime);
+				Quaternion rotation = Quaternion.LookRotation(_soteriaStatue.transform.position - this.transform.position);
+				this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.deltaTime);
 			}
 			else
 			{
