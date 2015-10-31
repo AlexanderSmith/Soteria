3e392a8f3e7a1b992c788b533287cd0a8f33c4df
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-10-31 11:55:38 -0500

    Updating scripted hide event script.  Need to push to create a new branch

--- SoteriaGame/Assets/Scripts/Events&Triggers/ScriptedHideEvent.cs
+++ SoteriaGame/Assets/Scripts/Events&Triggers/ScriptedHideEvent.cs
@@ -3,12 +3,28 @@ using System.Collections;
 
 public class ScriptedHideEvent : MonoBehaviour
 {
+	private Player _player;
+	private GameObject _enemy;
+	public Transform playerHidingSpot;
+	public Transform enemyRunAwaySpot;
+
+	void Start()
+	{
+		_player = GameDirector.instance.GetPlayer();
+		_enemy = GameObject.Find("ScriptedEnemy");
+	}
+
 	void OnTriggerEnter(Collider player)
 	{
-		if (Player.gameObject.tag == "Player")
+		if (player.gameObject.tag == "Player")
 		{
 			GameDirector.instance.GetPlayer().PlayerActionPause();
-
+			PlayEvent();
 		}
+	}
+
+	void PlayEvent()
+	{
+		//while(Vector3.Distance(_player.transform.position, playerHidingSpot.position) > 
 	}
 }
