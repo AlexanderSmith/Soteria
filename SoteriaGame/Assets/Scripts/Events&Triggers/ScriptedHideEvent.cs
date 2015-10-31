af90aaf3c35227c3c95adb4df74ca024688cb007
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-10-31 11:13:25 -0500

    Beginning to set up scripted hide event

---
+++ SoteriaGame/Assets/Scripts/Events&Triggers/ScriptedHideEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptedHideEvent : MonoBehaviour
+{
+	void OnTriggerEnter(Collider player)
+	{
+		if (Player.gameObject.tag == "Player")
+		{
+			GameDirector.instance.GetPlayer().PlayerActionPause();
+
+		}
+	}
+}
