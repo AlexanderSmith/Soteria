bf120d038cf84df57191e13140cc5aef309eb35d
unknown <csosa@14E511WS-1.cstcis.cti.depaul.edu>
2015-11-11 15:07:17 -0600

    Updated observatory district code to make walking through tile areas better.  Still need to add the spawner for when not on the tiles.  Updated soteria object in harbor/harbor respawn where their prefab connections were lost.  Put in Omar's audio source proxy fix.

--- SoteriaGame/Assets/Scripts/Events&Triggers/ObservatoryDistrict/TileLight.cs
+++ SoteriaGame/Assets/Scripts/Events&Triggers/ObservatoryDistrict/TileLight.cs
@@ -1,27 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObservatoryTile : MonoBehaviour
+public class TileLight : MonoBehaviour
 {
 	void Awake()
 	{
 		this.GetComponentInChildren<Light>().color = Color.cyan;
 	}
-
+	
 	void OnTriggerEnter(Collider player)
 	{
 		if (player.gameObject.tag == "Player")
 		{
-			GameDirector.instance.ChangeGameState(GameStates.HiddenTile);
 			this.GetComponentInChildren<Light>().color = Color.white;
 		}
 	}
-
+	
 	void OnTriggerExit(Collider player)
 	{
 		if (player.gameObject.tag == "Player")
 		{
-			GameDirector.instance.ChangeGameState(GameStates.Normal);
 			this.GetComponentInChildren<Light>().color = Color.cyan;
 		}
 	}
