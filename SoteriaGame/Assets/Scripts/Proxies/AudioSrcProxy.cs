1db9e7e32c7da7185003958fac8589b4fe4dd543
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-11-10 18:28:44 -0600

    Forgot this script for the audio manager.

--- SoteriaGame/Assets/Scripts/Proxies/AudioSrcProxy.cs
+++ SoteriaGame/Assets/Scripts/Proxies/AudioSrcProxy.cs
@@ -3,14 +3,26 @@ using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioSrcProxy : MonoBehaviour {
-
+	
+	public string AudioSourceID;
+	public bool IsDialogue;
+	
 	/// <summary>
 	/// Get the Audio Src from the objects and attaches it to the Audio Manager! then 
 	/// it can be controlled from there
 	/// </summary>
 	void Awake ()
-	{		
+	{	
+		//Temporary to avoid the code to break;
+		if (AudioSourceID == "")
+			AudioSourceID = this.gameObject.name;
+		
 		AudioSource AudioSrc =  this.gameObject.GetComponent<AudioSource>() as AudioSource;
-		GameDirector.instance.AttachAudioSource(AudioSrc, this.gameObject, this.gameObject.name);		
+		GameDirector.instance.AttachAudioSource(AudioSrc, this.gameObject, AudioSourceID);		
+		
+		if (IsDialogue)
+			GameDirector.instance.CollectAudioClipsForDialogue("Scene1",AudioSourceID);
+		else
+			GameDirector.instance.CollectAudioClips("Scene1",AudioSourceID);
 	}
 }
