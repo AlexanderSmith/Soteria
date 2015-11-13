a3f8e94426140d5174c72a90b11f929e7e736070
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-11-13 15:29:59 -0600

    Updating prefabs

--- SoteriaGame/Assets/Scripts/Entities/O'Malley/HubMovements.cs
+++ SoteriaGame/Assets/Scripts/Entities/O'Malley/HubMovements.cs
@@ -9,8 +9,8 @@ public class HubMovements : MonoBehaviour
 	private int _patrolIndex = 0;
 	private float _patrolTimer = 0.0f;
 	public float waitTime = 5.0f;
-
-
+	
+	
 	// Use this for initialization
 	void Start ()
 	{
@@ -21,32 +21,42 @@ public class HubMovements : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (patrolLocations.Length > 0)
+		if (!GameDirector.instance.isDialogueActive())
 		{
-			_anim.SetBool ("Moving", true);
-			if (_agent.remainingDistance < _agent.stoppingDistance)
+			_agent.Resume();
+			if (patrolLocations.Length > 0)
+			{
+				_anim.SetBool ("Moving", true);
+				if (_agent.remainingDistance < _agent.stoppingDistance)
+				{
+					_anim.SetBool ("Moving", false);
+					_patrolTimer += Time.deltaTime;
+					if (_patrolTimer >= waitTime)
+					{
+						if (_patrolIndex == patrolLocations.Length - 1)
+						{
+							_patrolIndex = 0;
+						}
+						else
+						{
+							_patrolIndex++;
+						}
+						_patrolTimer = 0.0f;
+					}
+				}
+				
+				_agent.destination = patrolLocations [_patrolIndex].position;
+			}
+			else
 			{
 				_anim.SetBool ("Moving", false);
-				_patrolTimer += Time.deltaTime;
-				if (_patrolTimer >= waitTime)
-				{
-					if (_patrolIndex == patrolLocations.Length - 1)
-					{
-						_patrolIndex = 0;
-					}
-					else
-					{
-						_patrolIndex++;
-					}
-					_patrolTimer = 0.0f;
-				}
 			}
-			
-			_agent.destination = patrolLocations [_patrolIndex].position;
 		}
 		else
 		{
 			_anim.SetBool ("Moving", false);
-		}	
+			_patrolTimer = 0.0f;
+			_agent.Stop();
+		}
 	}
 }
