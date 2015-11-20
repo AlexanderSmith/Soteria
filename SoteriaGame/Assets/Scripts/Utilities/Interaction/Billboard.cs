2195f62adbd4da96794fa8156420af76ba5c5fa5
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-11-20 17:10:35 -0600

    Hub pass 4 objective working properly.  Billboarding added hub interaction buttons.

---
+++ SoteriaGame/Assets/Scripts/Utilities/Interaction/Billboard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+	
+	
+	// Use this for initialization
+	public void Awake () 
+	{
+	}
+	
+	// Update is called once per frame
+	public void Update () 
+	{
+		transform.rotation = Camera.main.transform.rotation;
+	}
+}
