34111de09970db0576999774888646678abd3d00
JimN23 <jimncomputer23@aim.com>
2015-11-21 09:24:41 -0600

    Pls no breako, added some prefabs for dialogue

---
+++ SoteriaGame/Assets/Scripts/Events&Triggers/DialogueInteractions/MusicIntroDialogueP3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicIntroDialogueP3 : MonoBehaviour
+{
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			GameDirector.instance.GetPlayer().PlayerActionPause();
+			GameDirector.instance.SetupDialogue("AnaDistEnterVOMUSICp3", AudioID.None);
+			GameDirector.instance.StartDialogue();
+		}
+	}
+	
+	void OnTriggerStay(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			if (!GameDirector.instance.isDialogueActive())
+			{
+				this.gameObject.GetComponent<BoxCollider>().enabled = false;
+				GameDirector.instance.StopAudioClip(AudioID.BackgroundIntro);
+				GameDirector.instance.ChangeVolume(AudioID.BackgroundHarbor, 1f);
+			}
+		}
+	}
+}
