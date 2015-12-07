f4ee673017fa3d6acd3b3f71fcfee61b42a19dac
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-12-07 11:58:40 -0600

    Ana can remove suit after visiting all three puzzles with suit on.  Game flow branch to remove suit early not implemented.

---
+++ SoteriaGame/Assets/Scripts/Events&Triggers/O'MalleyInteractions/SuitOff.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuitOff : Reaction
+{
+	public override void execute()
+	{
+		GameDirector.instance.SuitRemoved();
+		GameDirector.instance.AddGamePhase();
+		GameDirector.instance.ChangeObjective(GameObject.Find ("HubToMusic"));
+	}
+}
