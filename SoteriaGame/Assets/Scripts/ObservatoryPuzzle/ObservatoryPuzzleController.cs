8bf42d6a2a3d6a7ec74b3a47f4c61ac1c86a9c1c
Chris <CSOSA@14E511WS-5.cstcis.cti.depaul.edu>
2015-12-19 15:57:22 -0600

    Forgot to save script...

--- SoteriaGame/Assets/Scripts/ObservatoryPuzzle/ObservatoryPuzzleController.cs
+++ SoteriaGame/Assets/Scripts/ObservatoryPuzzle/ObservatoryPuzzleController.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ObservatoryPuzzleController : MonoBehaviour {
+public class ObservatoryPuzzleController : MonoBehaviour
+{
+	LinkedList<GameObject> lList;
+	LinkedListNode<GameObject> prev;
+	LinkedListNode<GameObject> next;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
