4cd3f743fba2d890a9f0c4337eed469556018643
Chris <sosaca54@yahoo.com>
2016-02-12 10:17:15 -0600

    didn't finish coding door encounter whispers

--- SoteriaGame/Assets/Scripts/ObservatoryPuzzle/DoorEncounters.cs
+++ SoteriaGame/Assets/Scripts/ObservatoryPuzzle/DoorEncounters.cs
@@ -3,12 +3,31 @@ using System.Collections;
 
 public class DoorEncounters : MonoBehaviour
 {
+	public GameObject _thisEncounter;
+
 	void OnTriggerEnter(Collider player)
 	{
 		if (player.gameObject.tag == "Player")
 		{
 			GameDirector.instance.ObsPuzzleEncounter();
 			this.GetComponent<BoxCollider>().enabled = false;
+			//ObsPuzzleWhisperSetup();
 		}
+	}
+
+//	void OnTriggerStay(Collider player)
+//	{
+//		if (player.gameObject.tag == "Player")
+//		{
+//			GameDirector.instance.ObsPuzzleEncounter();
+//			this.GetComponent<BoxCollider>().enabled = false;
+//		}
+//	}
+
+	void ObsPuzzleWhisperSetup()
+	{
+		GameDirector.instance.GetPlayer().PlayerActionPause();
+		GameDirector.instance.SetupDialogue("WhispersMusicPuzzleActivation");
+		GameDirector.instance.StartDialogue();
 	}
 }
