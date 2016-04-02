1d5eccce4f41a421656b7b70e5676b6199528301
dkim828 <david.pg.kim@gmail.com>
2016-04-02 14:31:44 -0500

    Added Scripts for skip line

---
+++ SoteriaGame/Assets/Scripts/Utilities/Dialogue/DialogueSkipController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueSkipController : MonoBehaviour 
+{
+	public Sprite regular_button_IMG;
+	public Sprite highlighted_button_IMG;
+
+	private Image imageScript;
+
+	void Start()
+	{
+		imageScript = this.gameObject.GetComponent<Image> ();
+		imageScript.overrideSprite = this.regular_button_IMG;
+	}
+
+	public void SkipDialogue()
+	{
+		GameDirector.instance.SkipLine ();
+//		imageScript.overrideSprite = this.regular_button_IMG;
+	}
+
+	public void HighlightButton()
+	{
+		imageScript.overrideSprite = this.highlighted_button_IMG;
+	}
+
+	public void RegularButton()
+	{
+		imageScript.overrideSprite = this.regular_button_IMG;
+	}
+}
