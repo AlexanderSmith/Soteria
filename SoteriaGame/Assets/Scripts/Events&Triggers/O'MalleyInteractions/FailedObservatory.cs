6b4000f85df13a35d1daa781167fa1da8a9e069b
FierceLlama <sosaca54@yahoo.com>
2016-05-01 10:22:20 -0500

    - Skip button moved up
    - Removed obstacles from theater in Hub3
    - Fixed O'Malley interaction after Obs puzzle fail

--- SoteriaGame/Assets/Scripts/Events&Triggers/O'MalleyInteractions/FailedObservatory.cs
+++ SoteriaGame/Assets/Scripts/Events&Triggers/O'MalleyInteractions/FailedObservatory.cs
@@ -5,13 +5,15 @@ public class FailedObservatory : MonoBehaviour
 {
 	private GameObject _oMalleyPrefab;
 	public Transform oMalleySpawnLoc;
+	//public GameObject tailorLight;
 	
 	void Awake()
 	{
 		_oMalleyPrefab = GameObject.Find ("O'MalleyFailedObservatory");
 		_oMalleyPrefab.transform.position = oMalleySpawnLoc.position;
+		//tailorLight.GetComponent<Light> ().enabled = false;
 		// Testing
-		//GameDirector.instance.FirstTimeObservatoryPuzzle();
+//		GameDirector.instance.FirstTimeObservatoryPuzzle();
 	}
 	
 	void Start()
