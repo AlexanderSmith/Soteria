6629e9ab3d6bdfa080de174ae2fe29338566c517
Chris <sosaca54@yahoo.com>
2016-06-17 20:26:47 -0500

    Lighting updated for all scenes.  SKybox location moved

---
+++ SoteriaGame/Assets/Scripts/Utilities/TooltipStringFix.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TooltipStringFix : MonoBehaviour
+{
+    string text;
+
+    void Update()
+    {
+        text = this.GetComponent<Text>().text;
+
+        if (text.Length > 0)
+        { 
+            text = text.Replace('$', '\n');
+
+            this.GetComponent<Text>().text = text;
+        }
+    }	
+}
